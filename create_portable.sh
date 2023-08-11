16a0f68cca0769cf9f8f2d71219205acf6827127
tenkeyless <blivme84@naver.com>
2023-08-11 14:55:53 +0900

    * Add create for portable

---
+++ create_portable.sh
@@ -0,0 +1,13 @@
+nativefier \
+--name "VIBE" \
+"https://vibe.naver.com" \
+--icon "vibe_icon.png" \
+--single-instance \
+--title-bar-style 'hiddenInset' \
+--inject vibe_script.js \
+--inject vibe_drag.css \
+--tray \
+--background-color "#000000" \
+--global-shortcuts vibe_shortcuts_media.json \
+--disable-dev-tools \
+--portable
